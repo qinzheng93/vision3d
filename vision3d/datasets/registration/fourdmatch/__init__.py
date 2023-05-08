3097a0c736a9f1b074390977c8f8277c1f762f37
Zheng Qin <clavichord93@hotmail.com>
2023-05-08 22:09:54 +0800

    fix dataset error

--- vision3d/datasets/registration/fourdmatch/__init__.py
+++ vision3d/datasets/registration/fourdmatch/__init__.py
@@ -1,5 +1,4 @@
 from .fourdmatch import FourDMatchPairDataset
-from .fourdmatch_graph import FourDMatchWithGraphPairDataset
 from .utils import (
     compute_nonrigid_inlier_ratio,
     compute_nonrigid_feature_matching_recall,
